098cbee50ca694a2fc2a1fcead6eec6a403f4749
Yusuf Rizal <yusufrizalh@gmail.com>
2021-11-22 15:01:21 +0700

    create-react-app

---
+++ simple-blog/src/App.js
@@ -0,0 +1,14 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h3>Welcome to React Inixindo!</h3>
+      </header>
+    </div>
+  );
+}
+
+export default App;
