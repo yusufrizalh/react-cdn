55389eeb86b2deb230c3eb8bd287617ff3523f2f
Yusuf Rizal <yusufrizalh@gmail.com>
2021-11-23 09:46:21 +0700

    variable and datatype

--- simple-blog/src/App.js
+++ simple-blog/src/App.js
@@ -1,11 +1,24 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
+  const title = "Welcome to React Inixindo";
+  const likes = 100;
+  const person = { name: "stephanie", email: "stephanie@email.com" }; // type object
+  const link = "https://www.inixindo.id";
   return (
     <div className="App">
       <header className="App-header">
-        <h3>Welcome to React Inixindo!</h3>
+        <h3>{title}</h3>
+        <p>Liked {likes} times.</p>
+
+        <p>
+          <a href={link}>Inixindo Site</a>
+        </p>
+
+        <p>{1000}</p>
+        <p>{"Hello Inixindo"}</p>
+        <p>{[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]}</p>
+        <p>{Math.random() * 10}</p>
       </header>
     </div>
   );
