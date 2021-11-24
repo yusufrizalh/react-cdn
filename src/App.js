40831d17a60fef5d91b2204105d40fc664638663
Yusuf Rizal <yusufrizalh@gmail.com>
2021-11-24 14:44:07 +0700

    blog details

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Navbar from "./Navbar";
 import Home from "./Home";
 import Create from "./Create";
+import BlogDetails from "./BlogDetails";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
@@ -15,6 +16,9 @@ function App() {
             </Route>
             <Route path="/create">
               <Create />
+            </Route>
+            <Route path="/blogs/:id">
+              <BlogDetails />
             </Route>
           </Switch>
         </div>
--- src/BlogList.js
+++ src/BlogList.js
@@ -1,12 +1,16 @@
+import { Link } from "react-router-dom";
+
 const BlogList = ({ blogs, title, handleDeleteBlog }) => {
   return (
     <div className="blog-list">
       <h2>{title}</h2>
       {blogs.map((blog) => (
         <div className="blog-preview" key={blog.id}>
-          <h2>{blog.title}</h2>
-          <img src={blog.avatar} width="40px" height="40px" alt="Avatar" />
-          <p>Writen by {blog.author}</p>
+          <Link to={`/blogs/${blog.id}`}>
+            <h2>{blog.title}</h2>
+            <img src={blog.avatar} alt="Avatar" width="40px" height="40px" />
+            <p>Writen by {blog.author}</p>
+          </Link>
         </div>
       ))}
     </div>
