c8d5e9332f290d71f0afe77f008e9e5d9116eb68
Yusuf Rizal <yusufrizalh@gmail.com>
2021-11-25 10:04:03 +0700

    tracking form input

--- src/Create.js
+++ src/Create.js
@@ -1,7 +1,40 @@
+import { useState } from "react";
+
 const Create = () => {
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
+  const [author, setAuthor] = useState("James");
+
   return (
     <div className="create">
       <h2>Add a New Blog</h2>
+      <form>
+        <label>Blog title: </label>
+        <input
+          type="text"
+          required
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <label>Blog body: </label>
+        <textarea
+          required
+          value={body}
+          onChange={(e) => setBody(e.target.value)}
+        ></textarea>
+        <label>Blog author: </label>
+        <select value={author} onChange={(e) => setAuthor(e.target.value)}>
+          <option value="Yusuf">Yusuf</option>
+          <option value="Rizal">Rizal</option>
+          <option value="James">James</option>
+        </select>
+        <button>Add Blog</button>
+        <br />
+        <br />
+        <p>{title}</p>
+        <p>{body}</p>
+        <p>{author}</p>
+      </form>
     </div>
   );
 };
