2b4ae04ee9534921791befd9454b5ce95f02a825
Yusuf Rizal <yusufrizalh@gmail.com>
2021-11-25 10:11:50 +0700

    prevent default form

--- src/Create.js
+++ src/Create.js
@@ -5,10 +5,16 @@ const Create = () => {
   const [body, setBody] = useState("");
   const [author, setAuthor] = useState("James");
 
+  const handleSubmit = (event) => {
+    event.preventDefault(); // untuk menghindari adanya refresh
+    const blog = { title, body, author };
+    console.log(blog);
+  };
+
   return (
     <div className="create">
       <h2>Add a New Blog</h2>
-      <form>
+      <form onSubmit={handleSubmit}>
         <label>Blog title: </label>
         <input
           type="text"
