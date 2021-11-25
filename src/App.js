45c2bd1ed41525c21862a6e76be8e9caa17bf8ba
Yusuf Rizal <yusufrizalh@gmail.com>
2021-11-25 11:24:28 +0700

    halaman not-found

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Navbar from "./Navbar";
 import Home from "./Home";
 import Create from "./Create";
 import BlogDetails from "./BlogDetails";
+import NotFound from "./NotFound";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
@@ -19,6 +20,9 @@ function App() {
             </Route>
             <Route path="/blogs/:id">
               <BlogDetails />
+            </Route>
+            <Route path="*">
+              <NotFound />
             </Route>
           </Switch>
         </div>
