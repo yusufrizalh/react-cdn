f5e35ea0cb2a78f4419b5771d5c81d41338922c5
Yusuf Rizal <yusufrizalh@gmail.com>
2021-11-25 09:45:28 +0700

    show blog-details

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,11 +1,26 @@
 import { useParams } from "react-router-dom";
+import UseFetch from "./UseFetch";
 
 const BlogDetails = () => {
   const { id } = useParams();
+  const {
+    data: blog,
+    isLoading,
+    error,
+  } = UseFetch("http://localhost:8001/blogs/" + id);
 
   return (
     <div className="blog-details">
-      <h2>Blog Details - {id}</h2>
+      {isLoading && <div>Loading.....</div>}
+      {error && <div>{error}</div>}
+      {blog && (
+        <article>
+          <h2>{blog.title}</h2>
+          <img src={blog.avatar} alt="Avatar" width="100px" height="100px" />
+          <p>Writen by {blog.author}</p>
+          <div>{blog.body}</div>
+        </article>
+      )}
     </div>
   );
 };
